1b844e03f6087cc6d811791924309f9710e19898
Капралов Александр <capralow@gmail.com>
2020-11-10 22:45:44 +0300

    Первая версия плагина

--- bundles/ru.capralow.dt.ssl.checks.attachablecommands_v3_1_1.ui/src/ru/capralow/dt/ssl/checks/internal/attachablecommands_v3_1_1/ui/ExternalDependenciesModule.java
+++ bundles/ru.capralow.dt.ssl.checks.attachablecommands_v3_1_1.ui/src/ru/capralow/dt/ssl/checks/internal/attachablecommands_v3_1_1/ui/ExternalDependenciesModule.java
@@ -5,6 +5,7 @@ package ru.capralow.dt.ssl.checks.internal.attachablecommands_v3_1_1.ui;
 
 import org.eclipse.core.runtime.Plugin;
 
+import com._1c.g5.v8.dt.core.platform.IBmModelManager;
 import com._1c.g5.v8.dt.core.platform.IV8ProjectManager;
 import com._1c.g5.wiring.AbstractServiceAwareModule;
 
@@ -20,6 +21,7 @@ public class ExternalDependenciesModule
     @Override
     protected void doConfigure()
     {
+        bind(IBmModelManager.class).toService();
         bind(IV8ProjectManager.class).toService();
     }
 
--- bundles/ru.capralow.dt.ssl.checks.attachablecommands_v3_1_1.ui/src/ru/capralow/dt/ssl/checks/internal/attachablecommands_v3_1_1/ui/quickfix/ObjectFormModuleQuickFix.java
+++ bundles/ru.capralow.dt.ssl.checks.attachablecommands_v3_1_1.ui/src/ru/capralow/dt/ssl/checks/internal/attachablecommands_v3_1_1/ui/quickfix/ObjectFormModuleQuickFix.java
@@ -7,8 +7,12 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Optional;
 
 import org.antlr.stringtemplate.StringTemplate;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.text.edits.InsertEdit;
 import org.eclipse.xtext.nodemodel.ICompositeNode;
 import org.eclipse.xtext.nodemodel.INode;
@@ -17,15 +21,27 @@ import org.eclipse.xtext.ui.editor.quickfix.Fix;
 import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
 import org.eclipse.xtext.validation.Issue;
 
+import com._1c.g5.v8.bm.core.IBmTransaction;
+import com._1c.g5.v8.bm.integration.AbstractBmTask;
+import com._1c.g5.v8.bm.integration.IBmModel;
 import com._1c.g5.v8.dt.bsl.model.Method;
 import com._1c.g5.v8.dt.bsl.model.Module;
 import com._1c.g5.v8.dt.bsl.model.RegionPreprocessor;
 import com._1c.g5.v8.dt.bsl.model.util.BslUtil;
 import com._1c.g5.v8.dt.bsl.ui.quickfix.AbstractExternalQuickfixProvider;
+import com._1c.g5.v8.dt.core.platform.IBmModelManager;
 import com._1c.g5.v8.dt.core.platform.IV8Project;
 import com._1c.g5.v8.dt.core.platform.IV8ProjectManager;
+import com._1c.g5.v8.dt.form.model.EventHandler;
+import com._1c.g5.v8.dt.form.model.ExtendedMethodCallType;
+import com._1c.g5.v8.dt.form.model.Form;
+import com._1c.g5.v8.dt.form.model.FormExtInfo;
+import com._1c.g5.v8.dt.form.model.util.ModelUtils;
+import com._1c.g5.v8.dt.mcore.Event;
 import com._1c.g5.v8.dt.metadata.mdclass.Configuration;
+import com._1c.g5.v8.dt.metadata.mdclass.ObjectBelonging;
 import com._1c.g5.v8.dt.metadata.mdclass.ScriptVariant;
+import com.google.common.collect.Iterables;
 import com.google.common.io.CharSource;
 import com.google.common.io.CharStreams;
 import com.google.common.io.Resources;
@@ -43,6 +59,7 @@ public class ObjectFormModuleQuickFix
     private static final String ON_CREATE_AT_SERVER_TEMPLATE_NAME_RU = "OnCreateAtServerRu.txt"; //$NON-NLS-1$
     private static String onCreateAtServerTemplateContentRu;
     private static final String ON_CREATE_AT_SERVER_TEMPLATE_NAME_EN = "OnCreateAtServerEn.txt"; //$NON-NLS-1$
+
     private static String onCreateAtServerTemplateContentEn;
 
     static
@@ -50,10 +67,10 @@ public class ObjectFormModuleQuickFix
         onCreateAtServerTemplateContentRu = readContents(getFileInputSupplier(ON_CREATE_AT_SERVER_TEMPLATE_NAME_RU));
         onCreateAtServerTemplateContentEn = readContents(getFileInputSupplier(ON_CREATE_AT_SERVER_TEMPLATE_NAME_EN));
     }
-
     private static final String ON_CREATE_AT_SERVER_CALL_TEMPLATE_NAME_RU = "OnCreateAtServerCallRu.txt"; //$NON-NLS-1$
     private static String onCreateAtServerCallTemplateContentRu;
     private static final String ON_CREATE_AT_SERVER_CALL_TEMPLATE_NAME_EN = "OnCreateAtServerCallEn.txt"; //$NON-NLS-1$
+
     private static String onCreateAtServerCallTemplateContentEn;
 
     static
@@ -63,10 +80,10 @@ public class ObjectFormModuleQuickFix
         onCreateAtServerCallTemplateContentEn =
             readContents(getFileInputSupplier(ON_CREATE_AT_SERVER_CALL_TEMPLATE_NAME_EN));
     }
-
     private static final String ON_READ_AT_SERVER_TEMPLATE_NAME_RU = "OnReadAtServerRu.txt"; //$NON-NLS-1$
     private static String onReadAtServerTemplateContentRu;
     private static final String ON_READ_AT_SERVER_TEMPLATE_NAME_EN = "OnReadAtServerEn.txt"; //$NON-NLS-1$
+
     private static String onReadAtServerTemplateContentEn;
 
     static
@@ -74,10 +91,10 @@ public class ObjectFormModuleQuickFix
         onReadAtServerTemplateContentRu = readContents(getFileInputSupplier(ON_READ_AT_SERVER_TEMPLATE_NAME_RU));
         onReadAtServerTemplateContentEn = readContents(getFileInputSupplier(ON_READ_AT_SERVER_TEMPLATE_NAME_EN));
     }
-
     private static final String ON_READ_AT_SERVER_CALL_TEMPLATE_NAME_RU = "OnReadAtServerCallRu.txt"; //$NON-NLS-1$
     private static String onReadAtServerCallTemplateContentRu;
     private static final String ON_READ_AT_SERVER_CALL_TEMPLATE_NAME_EN = "OnReadAtServerCallEn.txt"; //$NON-NLS-1$
+
     private static String onReadAtServerCallTemplateContentEn;
 
     static
@@ -107,7 +124,29 @@ public class ObjectFormModuleQuickFix
     }
 
     @Inject
+    private IBmModelManager bmModelManager;
+    @Inject
     private IV8ProjectManager projectManager;
+
+    private void addEventHandler(Form moduleForm, String eventName, String handlerName, IBmTransaction transaction)
+    {
+        boolean isExtension = moduleForm.getMdForm().getObjectBelonging() != ObjectBelonging.NATIVE;
+        Form editing = transaction.toTransactionObject(moduleForm);
+        FormExtInfo editingExtInfo = transaction.toTransactionObject(moduleForm.getExtInfo());
+        Event onCreateAtServerEvent =
+            Iterables.find(editing.getFormEvents(), event -> event.getName().equals(eventName), null);
+        List<EventHandler> eventHandlers = editing.getHandlers();
+        if (isExtension)
+            eventHandlers = editingExtInfo.getHandlers();
+        Optional<EventHandler> found = ModelUtils.findEventHanlder(onCreateAtServerEvent, eventHandlers);
+        if (!found.isPresent())
+        {
+            EventHandler handler = !isExtension ? ModelUtils.newEventHandler(handlerName, onCreateAtServerEvent)
+                : ModelUtils.newEventHandler(handlerName, onCreateAtServerEvent, ExtendedMethodCallType.AFTER);
+            ModelUtils.addEventHandler(handler, eventHandlers);
+        }
+
+    }
 
     @Fix(ObjectFormModuleValidator.ERROR_METHOD_EXECUTE_COMMAND_AT_SERVER_MISSING_EXPORT)
     public void executeCommandAtServerAddExport(final Issue issue, final IssueResolutionAcceptor acceptor)
@@ -129,6 +168,22 @@ public class ObjectFormModuleQuickFix
         acceptor.accept(issue, Messages.Error_ObjectFormModule_MethodOnCreateAtServerNotExists_Title,
             Messages.Error_ObjectFormModule_MethodOnCreateAtServerNotExists_Description, null,
             new ExternalQuickfixModification<>(issue, Module.class, module -> {
+
+                Form moduleForm = (Form)module.getOwner();
+                IBmModel model = bmModelManager.getModel(moduleForm);
+                if (model == null)
+                    return null;
+
+                model.createLocalContext(Messages.Error_ObjectFormModule_MethodOnCreateAtServerNotExists_Title).execute(
+                    new AbstractBmTask<IStatus>(Messages.Error_ObjectFormModule_MethodOnCreateAtServerNotExists_Title)
+                    {
+                        @Override
+                        public IStatus execute(IBmTransaction transaction, IProgressMonitor monitor)
+                        {
+                            addEventHandler(moduleForm, "OnCreateAtServer", "ПриСозданииНаСервере", transaction); //$NON-NLS-1$ //$NON-NLS-2$
+                            return Status.OK_STATUS;
+                        }
+                    });
 
                 IV8Project v8Project = projectManager.getProject(module);
                 Configuration configuration = MdUtils.getConfigurationForProject(v8Project);
